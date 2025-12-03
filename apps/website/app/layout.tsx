95f6b038ee0298a83f67f69fe0e731fcad31c61b
David Turner <daveturner@live.com>
2025-12-03 07:44:18 -0500

    feat(website): add community links + technical doc (#26)
    * add community links
    
    * add technical doc

--- apps/website/app/layout.tsx
+++ apps/website/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import DiscordSvg from "@/components/DiscordSvg";
+import GithubSvg from "@/components/GithubSvg";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -20,9 +23,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`} >
+
+        <div className="flex justify-end gap-3 py-5 px-4 wrapper">
+          <Link href="https://discord.gg/WUxpCb9z" target="_blank" aria-label="Join the Discord">
+            <DiscordSvg className="size-6" />
+          </Link>
+          <Link href="https://github.com/shinzonetwork" target="_blank" aria-label="Visit the GitHub repository">
+            <GithubSvg className="size-6" />
+          </Link>
+        </div>
+
         {children}
 
         <footer className="py-10">
+          <div className="flex justify-center gap-3 py-5 px-4 wrapper">
+            <Link href="https://discord.gg/WUxpCb9z" target="_blank" aria-label="Join the Discord">
+              <DiscordSvg className="size-6" />
+            </Link>
+            <Link href="https://github.com/shinzonetwork" target="_blank" aria-label="Visit the GitHub repository">
+              <GithubSvg className="size-6" />
+            </Link>
+          </div>
+
           <p className="text-center text-sm">©2025 Shinzō, All Rights Reserved.</p>
         </footer>
       </body>
