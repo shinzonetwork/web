2265966c0f41d6c1fbfa802fad03f27107918518
Alex Cheng <alexander.cheng@utexas.edu>
2025-12-11 15:56:43 -0600

    hotfix

--- apps/webapp/pages/healthcheck.tsx
+++
@@ -1,7 +0,0 @@
-// apps/webapp/pages/healthcheck.tsx
-
-export default function Healthcheck() {
-  // This route exists only to force Next to emit pages-manifest.json
-  // It won't interfere with your / (root) app route.
-  return null;
-}
---
+++ apps/webapp/src/pages/healthcheck.tsx
@@ -0,0 +1,7 @@
+// apps/webapp/src/pages/healthcheck.tsx
+
+export default function Healthcheck() {
+  // Exists only to force Next to emit pages-manifest.json
+  return null;
+}
+
