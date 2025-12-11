f1cc1423f821d48c542db7f673131505ab25d59b
Alex Cheng <alexander.cheng@utexas.edu>
2025-12-11 15:53:13 -0600

    dummy healthcheck page to force next to emit pages-manifest.json

---
+++ apps/webapp/pages/healthcheck.tsx
@@ -0,0 +1,7 @@
+// apps/webapp/pages/healthcheck.tsx
+
+export default function Healthcheck() {
+  // This route exists only to force Next to emit pages-manifest.json
+  // It won't interfere with your / (root) app route.
+  return null;
+}
