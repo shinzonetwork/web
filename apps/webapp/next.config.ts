981044420b2931798d4a2b17789583f750a2798c
Alex Cheng <alexander.cheng@utexas.edu>
2025-12-11 15:20:01 -0600

    address turbopack issue

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        // Prevent Turbopack from trying to bundle `thread-stream`
+        "thread-stream": false as any,
+      },
+    },
+  },
 };
 
 export default nextConfig;
