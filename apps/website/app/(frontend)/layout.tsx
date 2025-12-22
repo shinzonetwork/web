5962e901d3d1cd852f5ef5be995bca2b09bfa979
david-on-github <david@source.network>
2025-12-22 17:22:11 -0500

    add staging + fixes

--- apps/website/app/(frontend)/layout.tsx
+++ apps/website/app/(frontend)/layout.tsx
@@ -2,15 +2,11 @@ import BlockContainer from "@/components/block-container";
 import { cn } from "@/lib/utils";
 import ShinzoLogoFull from "@/public/shinzo-logo-full.svg";
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Inter } from 'next/font/google';
+import { Geist_Mono, Inter } from 'next/font/google';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import './styles.css';
-
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"], });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"], });
-const inter = Inter({ variable: "--font-inter", subsets: ["latin"], });
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -21,34 +17,31 @@ export const metadata: Metadata = {
     card: "summary_large_image",
   },
   robots: {
-    index: false,
-    follow: false,
+    index: process.env.ALLOW_INDEXING === "true",
+    follow: process.env.ALLOW_FOLLOWING === "true",
   },
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
-  const fonts = `${geistSans.variable} ${geistMono.variable} ${inter.variable}`;
 
   return (
-    <html lang="en">
-      <body className={cn(fonts, `min-h-screen flex flex-col`)} >
-        <header className="sticky top-0 z-50 bg-background">
-          <BlockContainer className="flex items-center min-h-header-h">
-            <Link href="/blog"><Image src={ShinzoLogoFull} alt="Shinzō" className="w-[150px] lg:w-[255px]" aria-label="Shinzo" /></Link>
-          </BlockContainer>
-        </header>
+    <div className={cn(`min-h-screen flex flex-col`)} >
+      <header className="sticky top-0 z-50 bg-background">
+        <BlockContainer className="flex items-center min-h-header-h">
+          <Link href="/blog"><Image src={ShinzoLogoFull} alt="Shinzō" className="w-[150px] lg:w-[255px]" aria-label="Shinzo" /></Link>
+        </BlockContainer>
+      </header>
 
-        <main>
-          {children}
-        </main>
+      <main>
+        {children}
+      </main>
 
-        <footer className="mt-auto">
-          <BlockContainer className="py-10">
-            <p className="text-px-12 font-mono">© {new Date().getFullYear()} Shinzō</p>
-          </BlockContainer>
-        </footer>
-      </body>
-    </html>
+      <footer className="mt-auto">
+        <BlockContainer className="py-10">
+          <p className="text-px-12 font-mono">© {new Date().getFullYear()} Shinzō</p>
+        </BlockContainer>
+      </footer>
+    </div>
   )
 }
