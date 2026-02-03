df1fe229466506133e7cebab4654464c380c2944
Max Korsunov <maxkors2014@gmail.com>
2026-02-03 22:22:20 +0400

    fix: payload envs

--- apps/website/payload/payload.config.ts
+++ apps/website/payload/payload.config.ts
@@ -62,7 +62,7 @@ export default buildConfig({
       }),
     ],
   }),
-  secret: cloudflare.env.PAYLOAD_SECRET || process.env.PAYLOAD_SECRET || "",
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
   },
@@ -82,7 +82,7 @@ export default buildConfig({
   email: resendAdapter({
     defaultFromAddress: "updates@web-mail.shinzo.network",
     defaultFromName: "Shinzo Website",
-    apiKey: cloudflare.env.RESEND_API_KEY || process.env.RESEND_API_KEY || "",
+    apiKey: process.env.RESEND_API_KEY || "",
   }),
 });
 
