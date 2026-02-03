a6ae07671bdefabfc76b2d23789b4648fdd45180
Max Korsunov <maxkors2014@gmail.com>
2026-02-03 20:13:20 +0400

    feat: add migration

--- apps/website/migrations/index.ts
+++ apps/website/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20251219_182153_add_posts from './20251219_182153_add_post
 import * as migration_20251222_232308_enable from './20251222_232308_enable';
 import * as migration_20251222_232332_update_posts from './20251222_232332_update_posts';
 import * as migration_20251223_171559_add_blog_landing_global from './20251223_171559_add_blog_landing_global';
+import * as migration_20260203_160129 from './20260203_160129';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20251223_171559_add_blog_landing_global.up,
     down: migration_20251223_171559_add_blog_landing_global.down,
-    name: '20251223_171559_add_blog_landing_global'
+    name: '20251223_171559_add_blog_landing_global',
+  },
+  {
+    up: migration_20260203_160129.up,
+    down: migration_20260203_160129.down,
+    name: '20260203_160129'
   },
 ];
