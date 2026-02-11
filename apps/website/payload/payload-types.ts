88956b2ddb49e0000a541dd5c86e23239b0866f9
Max Korsunov <maxkors2014@gmail.com>
2026-02-11 19:59:15 +0400

    fix: update payload dependencies

--- apps/website/payload/payload-types.ts
+++ apps/website/payload/payload-types.ts
@@ -108,9 +108,7 @@ export interface Config {
     blogLanding: BlogLandingSelect<false> | BlogLandingSelect<true>;
   };
   locale: null;
-  user: User & {
-    collection: 'users';
-  };
+  user: User;
   jobs: {
     tasks: unknown;
     workflows: unknown;
@@ -157,6 +155,7 @@ export interface User {
       }[]
     | null;
   password?: string | null;
+  collection: 'users';
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
