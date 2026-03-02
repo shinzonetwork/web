4cd9921a4e1feedeab64c0844f11367988bc1468
Max Korsunov <maxkors2014@gmail.com>
2026-03-02 20:07:16 +0400

    fix: update rpc url

--- apps/faucet/lib/shared/envs.ts
+++ apps/faucet/lib/shared/envs.ts
@@ -3,4 +3,4 @@ export const RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY ?? '';
 
 export const FAUCET_PRIVATE_KEY   = process.env.FAUCET_PRIVATE_KEY ?? '';
 
-export const SHINZO_RPC = process.env.NEXT_PUBLIC_SHINZO_RPC ?? 'http://rpc.devnet.shinzo.network:26657';
+export const SHINZO_RPC = process.env.NEXT_PUBLIC_SHINZO_RPC ?? 'http://rpc.devnet.shinzo.network:8545';
