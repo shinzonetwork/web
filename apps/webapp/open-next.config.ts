536cf765bc09f61e566ad47255df9b5c3af69a07
Max Korsunov <maxkors2014@gmail.com>
2026-03-26 18:14:24 +0400

    fix: build script

--- apps/webapp/open-next.config.ts
+++ apps/webapp/open-next.config.ts
@@ -1,3 +1,6 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
 
-export default defineCloudflareConfig();
+export default {
+  buildCommand: "pnpm build:next",
+  ...defineCloudflareConfig(),
+};
