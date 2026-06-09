e0dfccbcffc602c2381c9fc1e174deed66a335b3
Max Korsunov <maxkors2014@gmail.com>
2026-06-09 18:26:20 +0400

    feat(explorer): install shinzohub package and remove syncing script

--- apps/explorer/lib/shared/database/db.ts
+++
@@ -1,7 +0,0 @@
-import { cache } from "react";
-import { getCloudflareContext } from "@opennextjs/cloudflare";
-
-export const getDb = cache(() => {
-  const { env } = getCloudflareContext();
-  return env.shinzohub_explorer;
-});
