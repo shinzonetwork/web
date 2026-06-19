50b7950bb43cc02877bb27d50061a1a9aaf2654d
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2026-06-19 16:14:29 -0400

    blocking V2 home page fro v1

--- apps/webapp/src/page-components/home/ui/page.tsx
+++ apps/webapp/src/page-components/home/ui/page.tsx
@@ -1,7 +1,13 @@
+import { isRegistrationV2 } from "@/shared/lib";
 import { Header } from "@/widget";
 import { Home } from "@/features/home/ui/home";
+import { redirect } from "next/navigation";
 
 export function HomePage() {
+  if (!isRegistrationV2()) {
+    return redirect("/registration");
+  }
+
   return (
     <>
       <Header />
