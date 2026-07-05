1c1f0db8ff05003e5b30f19e624b91395a1a046d
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2026-07-05 16:44:26 -0400

    moving Generator as the first link in header

--- apps/webapp/src/widget/menu/ui/header-menu.tsx
+++ apps/webapp/src/widget/menu/ui/header-menu.tsx
@@ -13,23 +13,23 @@ export function HeaderMenu() {
     <Menubar className="border-none shadow-none">
       <MenubarMenu>
         <MenubarTrigger className="flex items-center gap-2 text-lg text-muted-foreground">
-          <span>Host</span>
-          <ChevronDownIcon className="h-4 w-4" />
-        </MenubarTrigger>
-        <MenubarContent className="w-full min-w-[150px] bg-background shadow-md rounded-none">
-          <MenubarItem asChild className="text-lg text-muted-foreground">
-            <Link href="/host-registration">Registration</Link>
-          </MenubarItem>
-        </MenubarContent>
-      </MenubarMenu>
-      <MenubarMenu>
-        <MenubarTrigger className="flex items-center gap-2 text-lg text-muted-foreground">
           <span>Generator</span>
           <ChevronDownIcon className="h-4 w-4" />
         </MenubarTrigger>
         <MenubarContent className="w-full min-w-[150px] bg-background shadow-md rounded-none">
           <MenubarItem asChild className="text-lg text-muted-foreground">
             <Link href="/generator-assertion">Registration</Link>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger className="flex items-center gap-2 text-lg text-muted-foreground">
+          <span>Host</span>
+          <ChevronDownIcon className="h-4 w-4" />
+        </MenubarTrigger>
+        <MenubarContent className="w-full min-w-[150px] bg-background shadow-md rounded-none">
+          <MenubarItem asChild className="text-lg text-muted-foreground">
+            <Link href="/host-registration">Registration</Link>
           </MenubarItem>
         </MenubarContent>
       </MenubarMenu>
